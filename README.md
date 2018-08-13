acdfda4e84d3b6a99b1d9f3d2859ceaa71b60905
Albert Astals Cid <aacid@kde.org>
2018-08-13 10:20:35 +0200

    Make it more a ticket generator and not a badge generator

--- README.md
+++ README.md
@@ -1,13 +1,9 @@
-Generates sets of badges out of stdin pipe-separated '|' sets of data.
-
-The expected format of the input data is
-
-    Name1|Nickname1|Company1
-    Name2|Nickname2|Company2
-
-of course you can use anything else other than Name, Nickname and Company ;)
+Generates sets of tickets
 
 Usage:
-```bash
-python tagnames.py < data
+```mkdir -p output; python tagnames.py
 ```
+
+And then you will get a file in output/all_tickets.pdf that contains all the tickets
+
+Dependencies: inkscape, pdfunite
--- tagnames.py
+++ tagnames.py
@@ -1,71 +1,51 @@
 import sys
 import subprocess
 
-names = sys.stdin.read().split('\n')
+ticket_line1 = "Drink ticket"
+ticket_line2 = "Beer, Wine, non alcoholic"
+names = [ ticket_line1, ticket_line2 ]
+
+number_of_pages = 3
+tickets_per_page = 10
+
 currentNumber = 1
 
-page = 10
-blank = [ "____________________", "", "____________________" ]
+all_output_paths = [ ]
 
 def replaceName(ff, number, val):
     global currentNumber
     nombre = "%s - %s" % (val[0], str(currentNumber))
     ff=ff.replace('nombre'+number, nombre, 1)
-    ff=ff.replace('organizacion'+number, "Beer, Wine, non alcoholic", 1)
+    ff=ff.replace('organizacion'+number, val[1], 1)
     currentNumber = currentNumber + 1
     return ff
 
-for i in range(0,len(names)//page+1):
-    idx = i * page
+for i in range(0, number_of_pages):
+    idx = i * tickets_per_page
 
     f = open('akademy_ticket.svg')
     ff = f.read()
-    if idx<len(names):
-        ff = replaceName(ff, "1", names[idx].split('|'))
-    else:
-        ff = replaceName(ff, "1", blank)
-    if idx+1<len(names) and names[idx+1] != "":
-        ff = replaceName(ff, "2", names[idx+1].split('|'))
-    else:
-        ff = replaceName(ff, "2", blank)
-    if idx+2<len(names) and names[idx+2] != "":
-        ff = replaceName(ff, "3", names[idx+2].split('|'))
-    else:
-        ff = replaceName(ff, "3", blank)
-    if idx+3<len(names) and names[idx+3] != "":
-        ff = replaceName(ff, "4", names[idx+3].split('|'))
-    else:
-        ff = replaceName(ff, "4", blank)
-    if idx+4<len(names) and names[idx+4] != "":
-        ff = replaceName(ff, "5", names[idx+4].split('|'))
-    else:
-        ff = replaceName(ff, "5", blank)
-    if idx+5<len(names) and names[idx+5] != "":
-        ff = replaceName(ff, "6", names[idx+5].split('|'))
-    else:
-        ff = replaceName(ff, "6", blank)
-    if idx+6<len(names) and names[idx+6] != "":
-        ff = replaceName(ff, "7", names[idx+6].split('|'))
-    else:
-        ff = replaceName(ff, "7", blank)
-    if idx+7<len(names) and names[idx+7] != "":
-        ff = replaceName(ff, "8", names[idx+7].split('|'))
-    else:
-        ff = replaceName(ff, "8", blank)
-    if idx+8<len(names) and names[idx+8] != "":
-        ff = replaceName(ff, "9", names[idx+8].split('|'))
-    else:
-        ff = replaceName(ff, "9", blank)
-    if idx+9<len(names) and names[idx+9] != "":
-        ff = replaceName(ff, "10", names[idx+9].split('|'))
-    else:
-        ff = replaceName(ff, "10", blank)
+    ff = replaceName(ff, "1", names)
+    ff = replaceName(ff, "2", names)
+    ff = replaceName(ff, "3", names)
+    ff = replaceName(ff, "4", names)
+    ff = replaceName(ff, "5", names)
+    ff = replaceName(ff, "6", names)
+    ff = replaceName(ff, "7", names)
+    ff = replaceName(ff, "8", names)
+    ff = replaceName(ff, "9", names)
+    ff = replaceName(ff, "10", names)
     print('--')
 
-    outputpath = 'akaes/tags%d.svg' % i
+    outputpath = 'output/tickets%d.svg' % i
     towrite=open(outputpath, 'w')
     towrite.write(ff)
     towrite.close()
 
-    subprocess.call(["inkscape", outputpath, "--export-pdf=akaes/tags%d.pdf" % i])
+    subprocess.call(["inkscape", outputpath, "--export-pdf=output/tickets%d.pdf" % i])
+    all_output_paths.append("output/tickets%d.pdf" % i)
 
+pdfunite_call_args = all_output_paths
+pdfunite_call_args.insert(0, "pdfunite")
+pdfunite_call_args.append("output/all_tickets.pdf")
+subprocess.call(pdfunite_call_args)
